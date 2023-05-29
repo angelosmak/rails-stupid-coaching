e1c2379a2ff3b423a24b23eaff3cbe1618dd21d5
Angelos Makris <sade-1995@hotmail.com>
2023-05-29 09:54:49 +0300

    stupid-coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "ask", to: "pages#ask"
   get "answer", to: "pages#answer"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
